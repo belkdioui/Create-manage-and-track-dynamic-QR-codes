5538b360261705b9a80a6e84acac700f59c57821
bel-kdioui-111 <badre.elkdioui@gmail.com>
2024-01-26 18:44:54 +0100

    generate qr code

---
+++ generate_qr_code.py
@@ -0,0 +1,22 @@
+import qrcode
+
+# Function to generate QR code based on ID
+def generate_qr_code_from_id(user_id, file_path):
+    data_to_encode = f"ID: {user_id}"  # Using the ID in the QR code data
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data_to_encode)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(file_path)
+
+# Example usage
+user_id = "123456"  # Replace this with your actual user ID
+output_file_path = "q_code1.png"
+
+generate_qr_code_from_id(user_id, output_file_path)
