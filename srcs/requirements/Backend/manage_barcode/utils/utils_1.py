d68246abbe75a3d715637cfa48c3a86b00063d66
bel-kdioui-111 <badre.elkdioui@gmail.com>
2024-07-22 22:09:00 +0100

    still in signup error

--- srcs/requirements/Backend/manage_barcode/utils/utils_1.py
+++ srcs/requirements/Backend/manage_barcode/utils/utils_1.py
@@ -1,5 +1,9 @@
-    
+import re
+
 def check_errors(type, data):
+    EMAIL_REGEX = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+    PHONE_REGEX = r"(^(?:05|06|07)\d{8}$|^\+212\d{9}$)"
+
     errors = {}
     if type=="login":
         print("login")
@@ -8,6 +12,10 @@ def check_errors(type, data):
             errors["err_fname"]="First Name invalid "
         if any(char.isdigit() for char in data["lname"]):
             errors["err_lname"]="Last Name invalid "
+        if not re.match(EMAIL_REGEX, data["email"]):
+            errors["err_email"]="email invalid "
+        if not re.match(PHONE_REGEX, data["tel"]):
+            errors["err_tel"]="Phone Number invalid "
     return errors
     # def check_register(data):
     #     def check_if_emaail_is_valdi(email):
