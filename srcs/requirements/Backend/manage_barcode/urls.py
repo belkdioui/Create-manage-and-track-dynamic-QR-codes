49506de5e2ba0a2a284595c36b7ccb9d04d62a30
Mohamed Baanni <mbaanni@c1r3p5.1337.ma>
2024-08-07 18:15:21 +0100

    fixed login logic

--- srcs/requirements/Backend/manage_barcode/urls.py
+++ srcs/requirements/Backend/manage_barcode/urls.py
@@ -13,19 +13,18 @@ urlpatterns = [
 
     path('logout/', views.logout_api, name='logout'),
     path('profile/', views.profile, name='profile'),
-    
+    path('resend_email/', EmailVerification.reverify_email, name='reverify_email'),
     path('reset_password/', EmailVerification.forgot_password, name='forgot_password'),
     
-    path('email_verification/<str:token>/', EmailVerification.verifie, name='verifie'),
-    path('email_verification/', EmailVerification.forgot_password, name='forgot_password_resend'),
-    path('reset_password/<str:token>/', EmailVerification.verifie, name='verifie'),
     
+    path('reset_password/<str:token>/', EmailVerification.verifie_password, name='verifie'),
+    path('email_verification/<str:token>/', EmailVerification.verifie_email, name='verifie_email'),
     path('update_password/<str:token>/', EmailVerification.save_new_password, name='savepassword'),
+
 
     path('delete_account/', Profile.delete_account, name='delete_account'),
 
     path('update_data/', Profile.update_data, name='update_data'),
     path('qr_scanner/', Qrscanner.qr_scanner, name='qr_scanner'),
     path('scanner/', Qrscanner.scanner, name='scanner'),
-
 ]
--- srcs/requirements/Backend/manage_barcode/utils/EmailVerification.py
+++ srcs/requirements/Backend/manage_barcode/utils/EmailVerification.py
@@ -12,6 +12,7 @@ from datetime import date
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 
+
 def send_email(request, db_user, reason):
     subject = "Email Verification"
     body = f"Hello {db_user.fname.capitalize()} {db_user.lname.capitalize()} click this link to verifie your email http://0.0.0.0:8000/{reason}/{db_user.token}"
@@ -27,68 +28,78 @@ def send_email(request, db_user, reason):
     server.login(sender_email, os.environ.get('EMAIL_HOST_PASSWORD'))
     server.sendmail(sender_email, recipient_list, message.as_string())
 
-def verifie(request, token):
+def verifie_password(request, token):
     if request.user.is_authenticated:
         return redirect('/')
     ctx = {}
+    ctx['errors'] = []
+    try:
+        FormData.objects.get(token=token)
+        print('daze mnhnaaaa')
+        print('daze mnhnaaaa')
+        print('daze mnhnaaaa')
+        return render(request, 'pages/reset_password.html', {'token':token})
+    except FormData.DoesNotExist:
+        ctx['errors'].append('Invalide token')
+    except Exception:
+        ctx['errors'].append('Error sending email')
+    ctx['sendmail'] = "sendmail"
+    return render(request, 'pages/reset_password.html', context=ctx)
+
+
+def verifie_email(request, token):
+    if request.user.is_authenticated:
+        return redirect('/')
+    ctx = {}
+    ctx['errors'] = []
     try:
         db_user = FormData.objects.get(token=token)
-        
-        if request.path.find("reset_password") == -1:
-            return render(request, 'pages/reset_password.html', {'verifie_password':'true', 'token': token+'/'})
-
         db_user.activated = True
         db_user.token = ''
         db_user.save()
-
         return redirect('/')
-    
     except FormData.DoesNotExist:
-        ctx['errors']['form_not_exist'] = 'Invalide token'
+        ctx['errors'].append('Invalide token')
     except Exception as e:
-            ctx['errors']['sending_mail'] = 'Error sending email'
+        ctx['errors'].append('Error sending email')
 
-    return render(request, 'pages/reset_password.html', context=ctx)
+    return render(request, 'pages/resend_email.html', context=ctx)
     
 
 def save_new_password(request, token):
     if request.user.is_authenticated:
         return redirect('/')
     ctx = {}
-
     if request.method == "POST":
         try:
             user_db = FormData.objects.get(token=token)
             
             new_password = request.POST.get('new_password')
             c_password = request.POST.get('c_password')
-
+            print(new_password)
+            print(c_password)
+            print(token)
             data = {'password':new_password, 'cpassword':c_password}
             ctx['errors'] = check_errors("reset_password", data)
-            
-            if(len(ctx['errors']) != 0):
-                ctx['verifie_password'] = 'true'
+
+            if(len(ctx['errors'])):
+                print("eeerrrrrrrrrrr")
+                print("eeerrrrrrrrrrr")
                 return render(request, 'pages/reset_password.html', context=ctx)
 
             user = User.objects.get(username=user_db.email)
             user.set_password(new_password)
             user.save()
-
             user_db.token = ''
             user_db.save()
 
         except User.DoesNotExist:
-            ctx['errors']['user_error'] = 'Enter valid email'
+            ctx['errors'] = {'user_error':'Enter valid email'}
         except FormData.DoesNotExist:
-            ctx['errors']['email'] = 'Enter valid email'
+            ctx['errors']= {'email':'Enter valid email'}
         except Exception as e:
-            ctx['errors']['exception'] = f"Error saving password: {e}"
-
-        if(len(ctx['errors']) != 0):
-            ctx['verifie_password'] = 'true'
-            return render(request, 'pages/reset_password.html', context=ctx)
-        
-    return redirect('/')
+            ctx['errors']={'exception':f"Error saving password: {e}"}
+    return render(request, 'pages/login.html', context=ctx)
 
 def forgot_password(request):
     if request.user.is_authenticated:
@@ -100,15 +111,38 @@ def forgot_password(request):
         try:
             email = request.POST.get('email')
             db_user = FormData.objects.get(email=email)
+            if not db_user.activated:
+                ctx['errors'].append("unverified email")
+                return render(request, 'pages/login.html', context=ctx)
             db_user.token = hashlib.sha256((db_user.email + (str)(date.today())).encode("utf-8")).hexdigest()
             db_user.save()
             send_email(request, db_user, 'reset_password')
-            ctx['success'] = 'Email has been sent.'
-
+            ctx['success'] = 'Check you email for verification link.'
+            return render(request, 'pages/login.html', context=ctx)
         except FormData.DoesNotExist:
             ctx['errors'].append("This email is not registred.")
         except Exception:
             ctx['errors'].append("Error during sending email")
-    ctx['verifie_email'] = 'true'
+    ctx['sendmail'] = "sendmail"
     return render(request, 'pages/reset_password.html', context=ctx)
 
+
+def reverify_email(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    ctx= {}
+    if request.method == "POST":
+        ctx['errors'] = []
+        try:
+            email = request.POST.get('email')
+            db_user = FormData.objects.get(email=email)
+            db_user.token = hashlib.sha256((db_user.email + (str)(date.today())).encode("utf-8")).hexdigest()
+            db_user.save()
+            send_email(request, db_user, 'email_verification')
+            ctx['success'] = 'Email has been sent.'
+            return redirect ('/')
+        except FormData.DoesNotExist:
+            ctx['errors'].append("This email is not registred.")
+        except Exception:
+            ctx['errors'].append("Error during sending email")
+    return render(request, 'pages/resend_email.html', context=ctx)
