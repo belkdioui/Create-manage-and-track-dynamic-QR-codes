55276394bf335b6b86a4684b0f6cc569483a9c34
belkdioui <badre.elkdioui@gmail.com>
2024-08-07 11:11:32 +0100

    finish frontend of scheules & stops without media query

--- srcs/requirements/Backend/manage_barcode/static/js/Schedules_Stops.js
+++ srcs/requirements/Backend/manage_barcode/static/js/Schedules_Stops.js
@@ -10,5 +10,16 @@ function change_dir()
     terminusX.innerHTML = terminusY.innerHTML;
     first_child_of_td.innerHTML = terminusY.innerHTML;
     terminusY.innerHTML = temp;
+
+    const table = document.getElementById('data-table');
+    const rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+    for (let row of rows) {
+        const cells = Array.from(row.getElementsByTagName('td'));
+        cells.reverse();
+        row.innerHTML = '';
+        for (let cell of cells) {
+            row.appendChild(cell);
+        }
+    }
 }
 
